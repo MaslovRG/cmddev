b1ad66d404999fd5a182bf990bb288e7bbf7ea99
Al Maslov <Al Maslov>
2018-04-08 12:06:27 +0300

    Add example text description for developers

--- FileSync/FileSyncTests/Example1UnitTest.cs
+++ FileSync/FileSyncTests/Example1UnitTest.cs
@@ -4,13 +4,21 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace FileSyncTests
 {
     /// <summary>
-    /// Класс с методами-заглушками для проверки работоспособности
+    /// Класс-пример с методами-заглушками для проверки работоспособности
     /// </summary>
+    /// <description>
+    /// Название класса и всех его методов иметь в начале название разрабатываемой вами фичи
+    /// а также содержать информацию о том, что вы тестируете. 
+    /// Если тестируете что-то сложное, добавьте комментарий перед методом.
+    /// </description>
     [TestClass]
     public class Example1UnitTest
     {
+        /// <summary>
+        /// Тестируем сложную и важную вещь, проверяем равенство 1=1
+        /// </summary>
         [TestMethod]
-        public void Example1()
+        public void Example1ImportantThing()
         {
             Assert.AreEqual(1, 1); 
         }
