fc1dc080c852afd5cde31fc5ac6f7c73bcaa5407
Al Maslov <Al Maslov>
2018-04-08 11:37:41 +0300

    Add empty test classes

---
+++ FileSync/FileSyncTests/Example1UnitTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSyncTests
+{
+    /// <summary>
+    /// Класс с методами-заглушками для проверки работоспособности
+    /// </summary>
+    [TestClass]
+    public class Example1UnitTest
+    {
+        [TestMethod]
+        public void Example1()
+        {
+            Assert.AreEqual(1, 1); 
+        }
+    }
+}
---
+++ FileSync/FileSyncTests/Example2UnitTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileSyncTests
+{
+    /// <summary>
+    /// Второй пустой класс для проверки работоспособности 
+    /// </summary>
+    [TestClass]
+    public class Example2UnitTest
+    {
+        [TestMethod]
+        public void Example2()
+        {
+            Assert.AreEqual(1, 1); 
+        }
+    }
+}
