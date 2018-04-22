14be6e3854ba4fce51093a754c4577c3602c746f
Dmitry Frolov <mr.rikken@gmail.com>
2018-04-22 17:28:33 +0300

    #11 Finished Group class implementation.

--- FileSync/FileSyncSDK/Implementations/Group.cs
+++ FileSync/FileSyncSDK/Implementations/Group.cs
@@ -22,6 +22,8 @@ namespace FileSyncSDK.Implementations
                 throw new ArgumentException("Array must have all distinct values.", "files");
             if (folders != null && !IsAllDistinct(folders))
                 throw new ArgumentException("Array must have all distinct values.", "folders");
+            if (HaveIntersection(files, folders))
+                throw new ArgumentException("Arrays must not have intersection.");
 
             Name = name;
             LastSync = null;
@@ -34,14 +36,19 @@ namespace FileSyncSDK.Implementations
             ImportFromXml(element);
         }
 
-        private bool IsNullOrEmpty(string[] array)
+        private bool IsNullOrEmpty(IEnumerable<string> data)
         {
-            return array == null || array.Length == 0;
+            return data == null || !data.Any();
         }
 
-        private bool IsAllDistinct(string[] array)
+        private bool IsAllDistinct(IEnumerable<string> data)
         {
-            return array.Distinct().Count() == array.Length;
+            return data.Distinct().Count() == data.Count();
+        }
+
+        public bool HaveIntersection(IEnumerable<string> data1, IEnumerable<string> data2)
+        {
+            return data1 != null && data2 != null && data1.Intersect(data2).Any();
         }
 
         private IReadOnlyList<INamePath> CreateNamePathList(string[] filenames)
@@ -75,12 +82,72 @@ namespace FileSyncSDK.Implementations
 
         public XElement ExportToXml()
         {
-            throw new NotImplementedException();
+            var group = new XElement("group");
+            group.SetAttributeValue("name", Name);
+            if (LastSync != null)
+                group.SetAttributeValue("lastSync", LastSync.ToString());
+
+            group.Add(Files.Select(np => new XElement("file", new XAttribute("name", np.Name), new XText(np.Path))));
+            group.Add(Folders.Select(np => new XElement("folder", new XAttribute("name", np.Name), new XText(np.Path))));
+
+            return group;
         }
 
         public void ImportFromXml(XElement element)
         {
-            throw new NotImplementedException();
+            if (element == null)
+                throw new ArgumentNullException();
+            if (element.Name != "group")
+                throw new ArgumentException("XElement isn't \"group\" element.");
+            if (element.Attribute("name") == null)
+                throw new ArgumentException("XElement has no \"name\" attribute.");
+            if (!element.HasElements || (!HasSpecificElements(element, "file") && !HasSpecificElements(element, "folder")))
+                throw new ArgumentException("XElement has no \"file\" or \"folder\" child elements.");
+
+            List<INamePath> files = ImportNamePath(element, "file");
+            List<INamePath> folders = ImportNamePath(element, "folder");
+            if (HaveIntersection(files.Select(np => np.Path), folders.Select(np => np.Path)))
+                throw new ArgumentException("XElement data corrupted: files and folders path values intersection is not null.");
+
+            if (element.Attribute("lastSync") == null)
+                LastSync = null;
+            else
+                LastSync = DateTime.Parse(element.Attribute("lastSync").Value);
+
+            Name = element.Attribute("name").Value;
+            Files = files;
+            Folders = folders;
+        }
+
+        private List<INamePath> ImportNamePath(XElement parentElement, string elementsName)
+        {
+            List<INamePath> list = null;
+
+            try
+            {
+               list = parentElement.Elements()
+                    .Where(e => e.Name == elementsName)
+                    .Select(e => new NamePath(e.Attribute("name")?.Value, e.Value))
+                    .ToList<INamePath>();
+            }
+            catch(ArgumentNullException e)
+            {
+                throw new ArgumentException("XElement data corrupted: NamePath pair creation failed.", e);
+            }
+
+            if (list.Select(np => np.Name).Distinct().Count() != list.Count)
+                throw new ArgumentException("XElement data corrupted: Equal " + elementsName + " names found.");
+
+            if (list.Select(np => np.Path).Distinct().Count() != list.Count)
+                throw new ArgumentException("XElement data corrupted: Equal " + elementsName + " path data found.");
+
+            list = list.OrderBy(np => np.Name).ToList();
+            return list;
+        }
+
+        private bool HasSpecificElements(XElement element, string childElementName)
+        {
+            return element.Elements().Where(e => e.Name == childElementName).Any();
         }
 
         public void UpdateLastSync(DateTime dateTime)
