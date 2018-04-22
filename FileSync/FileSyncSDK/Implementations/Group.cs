b57b4cd3dfaedd5c959abb490dc129a77f40a7f7
Dmitry Frolov <mr.rikken@gmail.com>
2018-04-22 14:41:30 +0300

    #11 Group class basic constructor implemented.

--- FileSync/FileSyncSDK/Implementations/Group.cs
+++ FileSync/FileSyncSDK/Implementations/Group.cs
@@ -2,6 +2,7 @@
 using FileSyncSDK.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Xml;
@@ -12,7 +13,16 @@ namespace FileSyncSDK.Implementations
     {
         public Group(string name, string[] files, string[] folders)
         {
-            Name = string.IsNullOrEmpty(name) ? throw new ArgumentNullException("Name must not me null or empty.") : name;
+            if (string.IsNullOrEmpty(name))
+                throw new ArgumentNullException("Group name must not be null or empty string.");
+            if (IsNullOrEmpty(files) && IsNullOrEmpty(folders))
+                throw new ArgumentNullException("Files and folders arrays cannot be both null or empty.");
+            if (files != null && !IsAllDistinct(files))
+                throw new ArgumentException("Array must have all distinct values.", "files");
+            if (folders != null && !IsAllDistinct(folders))
+                throw new ArgumentException("Array must have all distinct values.", "folders");
+
+            Name = name;
             LastSync = null;
             Files = CreateNamePathList(files);
             Folders = CreateNamePathList(folders);
@@ -23,9 +33,35 @@ namespace FileSyncSDK.Implementations
             ImportFromXml(node);
         }
 
-        private IReadOnlyList<INamePath> CreateNamePathList(string[] files)
+        private bool IsNullOrEmpty(string[] array)
         {
-            throw new NotImplementedException();
+            return array == null || array.Length == 0;
+        }
+
+        private bool IsAllDistinct(string[] array)
+        {
+            return array.Distinct().Count() == array.Length;
+        }
+
+        private IReadOnlyList<INamePath> CreateNamePathList(string[] filenames)
+        {
+            if (filenames == null)
+                return new List<INamePath>();
+
+            var grouings = filenames.GroupBy(fn => Path.GetFileName(fn));
+            var singles = grouings.Where(g => g.Count() == 1).ToList();
+            var multiples = grouings.Where(g => g.Count() > 1).ToList();
+
+            List<INamePath> result = singles.Select(g => new NamePath(g.Key, g.FirstOrDefault())).ToList<INamePath>();
+            foreach (var grouping in multiples)
+            {
+                var list = grouping.ToList();
+                for (int i = 0; i < list.Count; ++i)
+                    result.Add(new NamePath(grouping.Key + "$" + i, list[i]));
+            }
+            result = result.OrderBy(np => np.Name).ToList();
+
+            return result;
         }
 
         public string Name { get; private set; }
