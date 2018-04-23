5cd1226b87185afe1d8dcc8d053fb1d3c435d994
Dmitry Frolov <mr.rikken@gmail.com>
2018-04-23 13:56:27 +0300

    #11 Began work on Session class implementation.

--- FileSync/FileSyncSDK/Implementations/Session.cs
+++ FileSync/FileSyncSDK/Implementations/Session.cs
@@ -1,18 +1,53 @@
-﻿using FileSyncSDK.Interfaces;
+﻿using CG.Web.MegaApiClient;
+using FileSyncSDK.Exceptions;
+using FileSyncSDK.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace FileSyncSDK.Implementations
 {
+    // TODO - progress reports
     internal class Session : ISession
     {
         public Session(string login, string password, string path, IProgress<IProgressData> progess = null)
+        {
+            try
+            {
+                this.progess = progess;
+                client = new MegaApiClient();
+                client.Login(login, password);
+                SetupWorkFolder(path);
+            }
+            catch (ArgumentNullException e)
+            {
+                throw new NoServiceSignInDataException(e);
+            }
+            catch (ApiException e)
+            {
+                throw new ServiceSignInFailedException(e);
+            }
+            finally
+            {
+                if (client.IsLoggedIn)
+                    client.Logout();
+            }
+        }
+
+        private void SetupWorkFolder(string path)
         {
             throw new NotImplementedException();
         }
 
         public ISettings GlobalSettings { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        private ISettings globalSettings = null;
+        private MegaApiClient client = null;
+        private INode workFolderNode = null;
+        private IProgress<IProgressData> progess = null;
+        private string tempFolderPath = null;
 
         public void DeleteGroup(IGroup group)
         {
@@ -21,7 +56,13 @@ namespace FileSyncSDK.Implementations
 
         public void Dispose()
         {
-            throw new NotImplementedException();
+            if (client.IsLoggedIn)
+                client.Logout();
+            if (tempFolderPath != null && Directory.Exists(tempFolderPath))
+            {
+                Directory.Delete(tempFolderPath);
+                tempFolderPath = null;
+            }
         }
 
         public void SyncronizeGroups(IGroup local, IGroup global)
