4160e00a43811d9839d2f4bd6bf684817d3d78ea
stmasha <stmasha>
2018-05-04 01:47:11 +0300

    #7 Tests for initialization of class Group created

--- FileSync/FileSyncTests/GroupTest.cs
+++ FileSync/FileSyncTests/GroupTest.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FileSyncSDK.Interfaces;
-using FileSyncSDK.Implementations; 
+using FileSyncSDK.Implementations;
+
+using System.Collections.Generic;
 
 namespace FileSyncTests
 {
     [TestClass]    
     public class GroupTest
     {
+        /* тесты для файла Group */
+
+            // тесты для инициализации Group
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void GroupContruction1()
@@ -22,6 +27,33 @@ namespace FileSyncTests
             Group group = new Group("name", null, null);
         }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GroupContruction3()
+        {
+            Group group = new Group("name", new string[] { "aaa", "aaa" }, null);
+        }
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GroupContruction4()
+        {
+            Group group = new Group("name", null, new string[] { "aaa", "aaa" });
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void GroupContruction5()
+        {
+            Group group = new Group("name", new string[] { "aaa" }, new string[] { "aaa"});
+        }
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
+        }
     }
 }
+
+// IEnumerable<string> files = new List<string> { "aaa", "bbb" };
