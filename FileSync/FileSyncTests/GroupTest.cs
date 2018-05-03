28e41fc3e341e7f13e903d08347f861db7f5d6ec
Al Maslov <Al Maslov>
2018-05-03 18:07:21 +0300

    #7 Add group construction test

---
+++ FileSync/FileSyncTests/GroupTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FileSyncSDK.Interfaces;
+using FileSyncSDK.Implementations; 
+
+namespace FileSyncTests
+{
+    [TestClass]    
+    public class GroupTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GroupContruction1()
+        {
+            Group group = new Group(null, null, null);              
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void GroupContruction2()
+        {
+            Group group = new Group("name", null, null);
+        }
+
+
+    }
+}
