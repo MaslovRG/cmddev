05d5e27688511dc1a0348c94b4d292915c8eedef
stmasha <stmasha>
2018-05-04 02:32:01 +0300

    #9 Tests for correct data set of class Group created

--- FileSync/FileSyncTests/GroupTest.cs
+++ FileSync/FileSyncTests/GroupTest.cs
@@ -56,34 +56,56 @@ namespace FileSyncTests
 
         // тест HaveIntersection
 
+        [TestMethod]
         public void HaveIntersection1()
         {
             Group group = new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
-            Assert.AreEqual(true, group.HaveIntersection(null, null));
+            Assert.AreEqual(false, group.HaveIntersection(null, null));
         }
 
+        [TestMethod]
         public void HaveIntersection2()
         {
             Group group = new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
-            Assert.AreEqual(true, group.HaveIntersection(new List<string> { "aaa", "bbb" }, null));
+            Assert.AreEqual(false, group.HaveIntersection(new List<string> { "aaa", "bbb" }, null));
         }
 
+        [TestMethod]
         public void HaveIntersection3()
         {
             Group group = new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
-            Assert.AreEqual(true, group.HaveIntersection(null, new List<string> { "aaa", "bbb" }));
+            Assert.AreEqual(false, group.HaveIntersection(null, new List<string> { "aaa", "bbb" }));
         }
 
+        [TestMethod]
         public void HaveIntersection4()
         {
             Group group = new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
             Assert.AreEqual(true, group.HaveIntersection(new List<string> { "aaa", "bbb" }, new List<string> { "aaa", "bbb" }));
         }
 
+        [TestMethod]
         public void HaveIntersection5()
         {
             Group group = new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
             Assert.AreEqual(false, group.HaveIntersection(new List<string> { "aaa", "bbb" }, new List<string> { "ccc" }));
+        }
+
+
+        // правильность ввода данных
+
+        [TestMethod]
+        public void Name()
+        {
+            Group group = new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
+            Assert.AreEqual("name", group.Name);
+        }
+
+        [TestMethod]
+        public void LastSync()
+        {
+            Group group = new Group("name", new string[] { "aaa" }, new string[] { "bbb" });
+            Assert.AreEqual(null, group.LastSync);
         }
     }
 }
